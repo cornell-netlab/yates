2b1a39b1bc5b7b4f0e4e63a1d91613222bb9d962
Robert Soulé <robert.soule@usi.ch>
2015-07-29 12:36:40 +0200

    Renamed Demands to use Kulfi prefix

--- simulate/Kulfi_Simulator.ml
+++ simulate/Kulfi_Simulator.ml
@@ -11,7 +11,7 @@ module VertexSet = Topology.VertexSet
        
 let () = Random.self_init ()
 
-let _ = Demands.create_random
+let _ = Kulfi_Demands.create_random
                 
 module Solver =  Kulfi_Routing.Make(Kulfi_MCF)
                 
@@ -27,8 +27,8 @@ let () =
             let label = Topology.vertex_to_label topo v in
             Node.device label = Node.Host) in
     let hosts = Topology.VertexSet.elements host_set in
-    let demand_matrix = Demands.create_sparse hosts 0.1 100 in
-    let pairs = Demands.get_demands demand_matrix in
+    let demand_matrix = Kulfi_Demands.create_sparse hosts 0.1 100 in
+    let pairs = Kulfi_Demands.get_demands demand_matrix in
     Printf.printf "# hosts = %d\n" (Topology.VertexSet.length host_set);
     Printf.printf "# pairs = %d\n" (List.length pairs);
     Printf.printf "# total vertices = %d\n" (Topology.num_vertexes topo);
