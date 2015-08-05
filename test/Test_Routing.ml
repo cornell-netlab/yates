5c4c28ae2a1c3dcdbb4ce4d2ee9c71767b8a9059
Robert Soulé <robert.soule@usi.ch>
2015-08-05 17:28:41 +0200

    Added Vlb test. There are 2 bugs that need to be fixed in VLB

--- test/Test_Routing.ml
+++ test/Test_Routing.ml
@@ -2,12 +2,12 @@ open Kulfi_Routing
 open Frenetic_Network
 open Net
 open Kulfi_Spf
+open Kulfi_Vlb
 open Kulfi_Types
 
 module VertexSet = Topology.VertexSet
 
-       
-let test_spf =
+let create_topology_and_demands = 
   let topo = Parse.from_dotfile "./data/3cycle.dot" in
   let host_set = VertexSet.filter (Topology.vertexes topo)
                                   ~f:(fun v ->
@@ -27,17 +27,31 @@ let test_spf =
                              hosts)
                 hosts;
     !lst
-  in 
+  in
   Printf.printf "# hosts = %d\n" (Topology.VertexSet.length host_set);
   Printf.printf "# pairs = %d\n" (List.length pairs);
   Printf.printf "# total vertices = %d\n" (Topology.num_vertexes topo);
+  (hosts,topo,pairs)
+                     
+let test_vlb =
+  let (hosts,topo,pairs) = create_topology_and_demands in
   let scheme = 
-    solve topo pairs SrcDstMap.empty in
+    Kulfi_Vlb.solve topo pairs SrcDstMap.empty in
+  let h1 = Array.get hosts 0  in 
+  let h2 = Array.get hosts 1  in
+  let paths = SrcDstMap.find (h1,h2) scheme in
+  (PathProbabilitySet.cardinal paths) == 2
+                     
+let test_spf =
+  let (hosts,topo,pairs) = create_topology_and_demands in
+  let scheme = 
+    Kulfi_Spf.solve topo pairs SrcDstMap.empty in
   let h1 = Array.get hosts 0  in 
   let h2 = Array.get hosts 1  in 
   let path = fst ( PathProbabilitySet.choose ( SrcDstMap.find (h1,h2) scheme ) ) in
   List.length path == 3
     
 TEST "spf" = test_spf = true
+TEST "vlb" = test_vlb = true
 
 
