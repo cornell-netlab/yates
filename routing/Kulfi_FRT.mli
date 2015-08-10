998ed4bfc54b60ff31a300d92076b0e7fc0fe38c
Robert Soulé <robert.soule@usi.ch>
2015-08-10 16:36:24 +0200

    Including Chris' MW and FRT code

---
+++ routing/Kulfi_FRT.mli
@@ -0,0 +1,40 @@
+open Frenetic_Network
+open Net
+
+module type FRT_TYPE = sig
+
+  type frt_tree
+  type routing_tree
+
+  type routing_edge = Topology.vertex * Topology.vertex
+  type routing_path = routing_edge list
+
+  val make_frt_tree : Topology.t -> frt_tree
+
+  val generate_rt : Topology.t -> frt_tree ->
+    Net.Topology.vertex list -> routing_tree
+
+  val usage_of_tree : routing_tree -> (Topology.edge * float) list
+
+  val get_path_halves : routing_tree -> Topology.vertex -> Topology.vertex ->
+    routing_path * routing_path
+
+  val get_path : routing_tree -> Topology.vertex ->
+    Net.Topology.vertex -> routing_path
+
+  val remove_cycles : NetPath.t -> NetPath.t
+
+  val routing_edges : routing_tree -> routing_edge list
+
+  val edge_to_physical : routing_tree -> routing_edge -> NetPath.t
+
+  val path_to_physical : routing_tree -> routing_edge list -> NetPath.t
+
+  val get_levels : frt_tree -> Topology.VertexSet.t list list
+
+  val write_frt : Topology.t -> frt_tree -> string -> unit
+  val write_rt : Topology.t -> routing_tree -> string -> unit
+
+end
+
+module FRT : FRT_TYPE
--- routing/Kulfi_Mw.mli
+++ routing/Kulfi_Mw.mli
@@ -1,4 +1,42 @@
+open Frenetic_Network
+open Net
 open Kulfi_Types
 
 val solve : topology -> demands -> scheme -> scheme
+       
+module type MW_INPUT = sig
 
+  type structure
+
+  (* Gets the next structure (e.g. path) that we are going to augment the
+   * flow by, along with the weight of that structure. *)
+  val select_structure : topology -> Topology.VertexSet.t -> (structure * float)
+
+  (* Gets the usages of every edge in the topology. *)
+  val usage_of_structure : topology -> structure -> (edge * float) list
+
+  (* Gets the weight of one edge in the topology. *)
+  val get_weight : topology -> edge -> float
+
+  (* Sets the weight of one edge in the topology. *)
+  val set_weight : topology -> edge -> float -> topology
+
+end
+
+module type MW_ALG = sig
+
+  type structure
+
+  val hedge : float -> float -> topology -> Topology.VertexSet.t
+    -> (edge, float) Hashtbl.t
+    -> (structure * float * topology * (edge, float) Hashtbl.t)
+
+  val hedge_iterations : float -> topology -> Topology.VertexSet.t ->
+    (int * (structure * float) list * topology)
+
+end
+
+module Make (Experts : MW_INPUT) : MW_ALG
+  with type structure = Experts.structure
+
+module RRTs : MW_ALG with type structure = Kulfi_FRT.FRT.routing_tree
