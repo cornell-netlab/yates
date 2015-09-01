b0dabd3194066fa30252bac3006a470e60f39577
Robert Soulé <robert.soule@usi.ch>
2015-09-01 13:12:15 +0200

    Can select many algorithms from the command line, iterate over them

--- simulate/Simulate_Driver.ml
+++ simulate/Simulate_Driver.ml
@@ -8,11 +8,10 @@ open RunningStat
 open ExperimentalData 
 open AutoTimer
        
-type solver_type = | Mcf | Vlb | Ecmp | Spf | Ak | NaS (*not a solver*)
-let solver_mode = ref NaS
-let topo_string = ref ""
-let iterations = ref 0
-let usage = Printf.sprintf "%s -s [solver] -t [topology] -i [iterations]\n" Sys.argv.(0)
+type solver_type = | Mcf | Vlb | Ecmp | Spf | Ak 
+
+let solver_mode = ref Mcf
+
 let solver_to_string (s:solver_type) : string =
   match s with 
   | Mcf -> "mcf" 
@@ -20,79 +19,82 @@ let solver_to_string (s:solver_type) : string =
   | Ecmp -> "ecmp"
   | Spf -> "spf" 
   | Ak -> "ak" 
-  | _ -> raise (Arg.Bad("Unknown solver"))
-
-let choose_solver = function
-  | "mcf" -> solver_mode := Mcf
-  | "vlb" -> solver_mode := Vlb
-  | "ecmp" -> solver_mode := Ecmp
-  | "spf" -> solver_mode := Spf
-  | "ak" -> solver_mode := Ak
-  | _ -> raise (Arg.Bad("Unknown solver"))
-
-let speclist = [
-  ("-s", Arg.Symbol (["mcf"; "vlb"; "ecmp"; "spf"; "ak"], 
-		    choose_solver), ": set solver strategy");
-  ("-t", Arg.Set_string topo_string, ": set topology file");
-  ("-i", Arg.Set_int iterations, ": set number of iterations");
-]
-
-let missing_args () = match (!solver_mode, !topo_string, !iterations) with
-  | (NaS,_,_) -> true
-  | (_,"",_) -> true
-  | (_,_,0) -> true
-  | _ -> false
-	
+		
 let select_algorithm solver = match solver with
   | Mcf -> Kulfi_Routing.Mcf.solve
-  | Vlb -> Kulfi_Routing.Mcf.solve
-  | Ecmp -> Kulfi_Routing.Mcf.solve
-  | Spf -> Kulfi_Routing.Mcf.solve
-  | Ak -> Kulfi_Routing.Mcf.solve
-  | _ -> assert false
+  | Vlb -> Kulfi_Routing.Vlb.solve
+  | Ecmp -> Kulfi_Routing.Ecmp.solve
+  | Spf -> Kulfi_Routing.Spf.solve
+  | Ak -> Kulfi_Routing.Ak.solve
 
-		 
-let main =
-  Arg.parse speclist print_endline usage;
-  if (missing_args ()) then Printf.printf "%s" usage
-  else
-    begin
-      Printf.printf "Kulfi Simulate";
-      let topo = Parse.from_dotfile !topo_string in
-	  let host_set = VertexSet.filter (Topology.vertexes topo)
-					  ~f:(fun v ->
-					      let label = Topology.vertex_to_label topo v in
-            Node.device label = Node.Host) in
-	  let hosts = Topology.VertexSet.elements host_set in
-	  let demand_matrix = Simulate_Demands.create_sparse hosts 0.1 100 in
-	  let pairs = Simulate_Demands.get_demands demand_matrix in
-	  Printf.printf "# hosts = %d\n" (Topology.VertexSet.length host_set);
-	  Printf.printf "# pairs = %d\n" (List.length pairs);
-	  Printf.printf "# total vertices = %d\n" (Topology.num_vertexes topo);
-	  let at = ref (make_auto_timer ()) in
-	  let times = ref (make_running_stat ()) in
-	  let solve = select_algorithm !solver_mode in
-	  let data = ref (make_data "Iteratives Vs Time") in
-	  let rec loop n = 
-	    if n > !iterations then
-	      ()
-	    else
-	      begin		
-		at := start !at ;
-		let _ = solve topo pairs SrcDstMap.empty in 
-		at := stop !at ;
-		times := push !times (get_time_in_seconds !at) ;
-		loop (n+1)
-	      end
-	  in
-	  loop 1;
-	  data := add_record !data (solver_to_string !solver_mode)
-			     {iteration = !iterations;
-			      time=(get_mean !times);
-			      time_dev=(get_standard_deviation !times); };
-	  Printf.printf "%s" (to_string !data "# solver\titer\ttime\tstddev" iter_vs_time_to_string) 
-			     
-    end
-		  
+let simulate (spec_solvers:solver_type list) (topology_file:string) (iterations:int) () : unit =
+  let topo = Parse.from_dotfile topology_file in
+  let host_set = VertexSet.filter (Topology.vertexes topo)
+				  ~f:(fun v ->
+				      let label = Topology.vertex_to_label topo v in
+				      Node.device label = Node.Host) in
+  let hosts = Topology.VertexSet.elements host_set in
+  let demand_matrix = Simulate_Demands.create_sparse hosts 0.1 100 in
+  let pairs = Simulate_Demands.get_demands demand_matrix in
+  Printf.printf "# hosts = %d\n" (Topology.VertexSet.length host_set);
+  Printf.printf "# pairs = %d\n" (List.length pairs);
+  Printf.printf "# total vertices = %d\n" (Topology.num_vertexes topo);
+  let at = ref (make_auto_timer ()) in
+  let times = ref (make_running_stat ()) in
+
+  let data = ref (make_data "Iteratives Vs Time") in
+
+  let rec outer algorithms = match algorithms with
+    | [] -> ()
+    | algorithm::rest ->
+       let solve = select_algorithm algorithm in
+       let rec inner n = 
+	 if n > iterations then
+	   ()
+	 else
+	   begin		
+	     at := start !at ;
+	     let _ = solve topo pairs SrcDstMap.empty in 
+	     at := stop !at ;
+	     times := push !times (get_time_in_seconds !at) ;
+
+	     data := add_record !data (solver_to_string algorithm)
+				{iteration = n;
+				 time=(get_mean !times);
+				 time_dev=(get_standard_deviation !times); };
+	     
+	     inner (n+1)
+	   end
+       in
+       inner 1;
+       outer rest
+  in
+  outer spec_solvers;
+  Printf.printf "%s" (to_string !data "# solver\titer\ttime\tstddev" iter_vs_time_to_string)
+		     
+let command =
+  Command.basic
+    ~summary:"Simulate run of routing strategies"
+    Command.Spec.(
+    empty
+    +> flag "-mcf" no_arg ~doc:" run mcf"
+    +> flag "-vlb" no_arg ~doc:" run vlb"
+    +> flag "-ecmp" no_arg ~doc:" run ecmp"
+    +> flag "-spf" no_arg ~doc:" run spf"
+    +> flag "-ak" no_arg ~doc:" run ak"
+    +> anon ("filename" %: string)
+    +> anon ("iterations" %: int)
+  ) (fun (mcf:bool) (vlb:bool) (ecmp:bool) (spf:bool) (ak:bool) (topology_file:string) (iterations:int) () ->
+     (* TODO(rjs) : how can I make this code uglier? *)
+     let algorithms = 
+       (if mcf then [Mcf] else [])@
+	 (if vlb then [Vlb] else [])@
+	   (if ecmp then [Ecmp] else [])@
+	     (if spf then [Spf] else [])@
+	       (if ak then [Ak] else []) in            
+     simulate algorithms topology_file iterations () )
+
+let main = Command.run command
+ 
 let _ = main 
 
