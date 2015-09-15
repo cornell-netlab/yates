797d667792705b1b6e10475003b53a923ffdeaf7
Praveen Kumar <praveenk@cs.cornell.edu>
2015-09-15 14:31:24 -0400

    [HW exp] clean up

--- experiments/testbed/common.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-KULFI_GIT_DIR=$HOME/git/kulfi
-ATLAS_KULFI_CONFIG=~/atlas-kulfi-configure
-LOG_DIR=~/log/
-ATLAS_ABILENE_SCRIPT=~/atlas-node-start-rep.sh
-ABILENE_DIR=~/atlas-abilene
-SYNC_DIR=~/sync
