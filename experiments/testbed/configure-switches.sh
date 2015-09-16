f2b7ba883ad3724d313368a560417dfbd16d451e
Praveen Kumar <praveenk@cs.cornell.edu>
2015-09-16 16:29:51 -0400

    install rule for agent network

--- experiments/testbed/configure-switches.sh
+++ experiments/testbed/configure-switches.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+SPLIT=$1
 for s in `seq 1 3`;
 do
-	ssh root@sw$s "bash -s" < ./switch/split/sw$s.sh
+	if ! [[ -z $SPLIT ]]; then
+		echo "splitting"
+		cat ~/.ssh/id_rsa.pub | ssh root@sw$s "mkdir .ssh && cat - > .ssh/authorized_keys"
+		ssh root@sw$s "bash -s" < ./switch/split/sw$s.sh
+	fi
 	ssh root@sw$s "bash -s" < ./switch/agent-network/sw$s.sh
 done
