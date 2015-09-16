786b71f48f039aadb962e2d4979d2ef60cd28fb5
Robert Soulé <robert.soule@usi.ch>
2015-09-16 13:17:43 +0200

    should fix compilation error

--- simulate/Simulate_Traffic.ml
+++ simulate/Simulate_Traffic.ml
@@ -8,7 +8,8 @@ let open_demands (file:string) : in_channel =
 let close_demands (ic:in_channel) : unit =
   close_in ic
 	  
-let get_demands (ic:in_channel) : demands =
+let get_demands (ic:in_channel) : (* TODO(rjs) We should return a 
+                                     Kulfi.demands, need to consult map *) =
   let line =
     try 
       input_line ic 
@@ -19,13 +20,14 @@ let get_demands (ic:in_channel) : demands =
   let entries = String.split line ~on:' ' in
   let size = Int.of_float (sqrt (Float.of_int (List.length entries))) in
   let demands = Array.make_matrix size size 0. in
-  ignore (for i = 0 to size do
-	    for j = 0 to size do
-	      let v = match (List.nth entries ((i * size) + j)) with
-		| Some x -> x
-		| None -> assert false in
-	      demands.(i).(j) <- Float.of_string v
-	    done
-	  done
-	 );
+  for i = 0 to size do
+    for j = 0 to size do
+      (* TODO: We don't need to build the matrix.
+          we can just populate a Kulfi_Types.demands directly *)      
+      let v = match (List.nth entries ((i * size) + j)) with
+	| Some x -> x
+	| None -> assert false in
+      demands.(i).(j) <- Float.of_string v
+    done
+  done;
   demands
