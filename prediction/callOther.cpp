3326ca7dba24fbdba8d2d47089a15eb7c021c645
Yang <callowbird@gmail.com>
2015-09-21 15:01:49 -0400

    minor changes

--- prediction/callOther.cpp
+++ prediction/callOther.cpp
@@ -2,41 +2,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
+int main(int argc, char ** argv)
 {
     FILE * fsize=fopen("numOfHosts.txt", "r");
     int n;
     system("mkdir matrix");
+    if (argc<2)
+    {
+        printf("error! too few args\n");
+        return 0;
+    }
 
     while (fscanf(fsize, "%i", &n))
     {
         char command[200];
-        sprintf(command, "./libpredict/bin/predict 2 1000 %i matrix/syn-%i 1.0", n, n);
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("*now running %s ********************************************88\n", command);
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
-        printf("********\n");
+        sprintf(command, "./libpredict/bin/predict 2 1000 %i matrix/syn-%i 1.0 %s", n, n, argv[1]);
+        printf("\n********\n");
+        printf("*now running %s \n", command);
+        printf("********\n\n\n");
         system(command);
     }
 
