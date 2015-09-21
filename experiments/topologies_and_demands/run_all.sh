d623243dc02c5f6ffeb98dc6dfaa02d2febbab26
Robert Soulé <robert.soule@usi.ch>
2015-09-21 16:30:09 +0200

    Added notes about what needs to be run

--- experiments/topologies_and_demands/run_all.sh
+++ experiments/topologies_and_demands/run_all.sh
@@ -14,4 +14,13 @@ do
     num_hosts=`grep mac $tf | wc -l`
     echo $num_hosts
     "${PREDICT} 2 1000 $num_hosts $prefix 1.0"
+
+    # STEP 1: mv files to correct names
+    
+    # STEP 2: Call Simulator
+  # ./Simulate_Driver.native data/topologies/3cycle.dot data/demands/3cycle_demands.txt data/predict/3cycle_predict.txt data/hosts/3cycle_hosts.txt 4 -all
+
+    # STEP 3: Call Simulator
+    # mv expData/CongestionVsIterations.dat expData/3cycle-CongestionVsIterations.dat
+    
 done
