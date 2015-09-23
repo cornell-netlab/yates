793ac185b161eea0b1bc5931e91a7ad14547d59d
Praveen Kumar <praveenk@cs.cornell.edu>
2015-09-22 22:28:25 -0400

    Updated sim scripts

--- experiments/topologies_and_demands/run_all.sh
+++ experiments/topologies_and_demands/run_all.sh
@@ -7,6 +7,8 @@ echo $EXP_DIR
 echo $CURR_DIR
 mkdir -p $EXP_DIR/results
 mkdir -p $EXP_DIR/hosts
+mkdir -p $CURR_DIR/expData
+mkdir -p ~/log
 TOPOLOGY_FILES=${TOPO_DIR}/*
 TM_DIR=${CURR_DIR}/prediction/scripts/synthetic_demands
 
@@ -24,6 +26,7 @@ do
     sed ':a;N;$!ba;s/\n/ /g' ${tf} | sed 's/\;/\n/g' | sed 's/{/\n/g' | grep host | awk '{print $1;}' | sort > $EXP_DIR/hosts/${prefix}.txt
     cmd="./Simulate_Driver.native -scalesyn ${tf} ${actual_demand} ${predicted_demand} $EXP_DIR/hosts/${prefix}.txt 1000 -all"
     echo $cmd
-    $cmd &  
+    $cmd &> ~/log/${prefix}.log &
 done
 
+watch -n 1 "ps aux | grep \"Simulate_Driver\" | grep -v \"grep\" | sort -rk 3,3 "
