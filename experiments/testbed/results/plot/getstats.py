603358d581182dd72dad8a96a17523a831d0e845
Praveen Kumar <praveenk@cs.cornell.edu>
2015-09-22 21:45:22 -0400

    Reorg files

--- experiments/testbed/results/plot/getstats.py
+++ experiments/testbed/results/plot/getstats.py
@@ -7,4 +7,4 @@ for row in X:
     undir = [sum(row[i:i+1]) for i in range(0, len(row)-1, 2)]
     mx = np.max(undir)
     mdn = np.median(undir)
-    print str(mx/125000000)  + " " + str(mdn/125000000)
+    print str(min(1.0, mx/125000000))  + " " + str(mdn/125000000)
