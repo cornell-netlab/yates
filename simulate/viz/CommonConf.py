6bda6e53444388c89e44b1dbd1153537e14fb0ec
Praveen Kumar <praveenk@cs.cornell.edu>
2016-01-31 20:46:36 -0500

    Update plotting scripts

--- simulate/viz/CommonConf.py
+++ simulate/viz/CommonConf.py
@@ -11,6 +11,8 @@ def setupMPPDefaults():
     pp.rcParams['axes.titlesize'] = 18
     pp.rcParams['axes.labelsize'] = 15
 
+algs = 'ecmp ksp mcf raeke vlb semimcfecmp semimcfksp semimcfmcfenv  semimcfraeke semimcfvlb semimcfmcfftenv spf optimalmcf'.split()
+
 def getLineMarkers():
   #return ['+-', '*-', 'x-', '<-', '>-', 'o-', 's-', 'd-', '^-', 'v-']
     # return [ r'$\lambda$',
@@ -20,10 +22,31 @@ def getLineMarkers():
     #          r'$\checkmark$',
     #           '<', '>']
     #return ['s', 'o', '<', '^', 'v', 'd', '.', '*', 'p'] *10
-    return ['s', 'o', '<', '^', 'v', 'd', '.', 's', 'o', '*', '<', '^', 'v', 'd', '.', '*', 'p'] *10
+    return ['<', '^', 'v', '>', '<', 'h', '.', 'o', '*', 'p', 'd', 'x'] *10
 
 def getLineColors():
-    return ['b','g','r','m','y','k', 'm', 'r', 'c', 'g', 'y', 'k'] * 10
+    return ['r','m', 'c', 'g', 'blue'] * 2 + ['coral', 'darkred'] * 4
 
 def getLineFormats():
-    return [':','-.', '--'] * 100
+    return [':']*5 + ['--']*6 + ['-.'] * 100
+
+def getLineMarkersDict():
+    res = dict()
+    marks = getLineMarkers()
+    for x in range(len(algs)):
+        res[algs[x]] = marks[x]
+    return res
+
+def getLineFormatsDict():
+    res = dict()
+    marks = getLineFormats()
+    for x in range(len(algs)):
+        res[algs[x]] = marks[x]
+    return res
+
+def getLineColorsDict():
+    res = dict()
+    marks = getLineColors()
+    for x in range(len(algs)):
+        res[algs[x]] = marks[x]
+    return res
