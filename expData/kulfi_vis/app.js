451ace77da84dc0b690ae671dcb6edc9ddcc74d4
Yang Yuan <yy528@cornell.edu>
2016-10-01 15:40:31 -0400

    refined version of the visualization. Put the files list into the files.json, and then run vis.html

--- expData/kulfi_vis/app.js
+++ expData/kulfi_vis/app.js
@@ -1,23 +1,24 @@
-﻿var width = 3000,
-    height = 1500;
+﻿var width = 800,
+    height = 400;
 
-function getanewgraph(filename ,base) {
+var linksTot=[];
+var nodesTot=[];
+var pathSet=[];
+var circleSet=[];
+var circleTextSet=[];
+var scalesSet=[];
+var svgSet=[];
+var forceSet=[];
+
+function getanewgraph(filename ,whichComp) {
     var links;
 
     var nodes = {};
     var counts = {};
     var selected_link = null;
-    var nodeRadius = 15;
-    var force;
+    var nodeRadius = 5;
     var thres=10;
-    var linksTot=[];
-    var nodesTot=[];
-    var bases=[];
-    var pathSet=[];
-    var circleSet=[];
-    var scalesSet=[];
-    var svgSet=[];
-    var curFile;
+    var curComp;
 
 
 
@@ -49,7 +50,7 @@ function getanewgraph(filename ,base) {
 
 
     function capacityForNode(d) {
-        return Math.log(d.capacity)*1.5;
+        return Math.log(d.capacity)*0.3;
     }
 
     function sqr(a)
@@ -57,16 +58,19 @@ function getanewgraph(filename ,base) {
         return a*a;
     }
     function tick() {
-        pathSet[curFile].selectAll('path').attr('d', function (d) {
+        pathSet[curComp].selectAll('path').attr('d', function (d) {
             if (counts[d.norm] == 1)
                 return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y;
             var len= Math.sqrt(sqr(d.source.x-d.target.x)+sqr(d.source.y-d.target.y));
-            var dr1 = (d.source.x+d.target.x)/2+(d.count)*100*(d.source.x-d.target.x)/len;
-            var dr2 = (d.source.y+d.target.y)/2+(d.count)*100*(d.target.y-d.source.y)/len;
+            var dr1 = (d.source.x+d.target.x)/2+(d.count)*35*(d.source.x-d.target.x)/len;
+            var dr2 = (d.source.y+d.target.y)/2+(d.count)*35*(d.target.y-d.source.y)/len;
             return "M" + d.source.x + "," + d.source.y + " Q " +dr1+","+dr2+" "+ d.target.x + "," + d.target.y;
         });
 
-        circleSet[curFile].selectAll('g').attr('transform', function (d) {
+        circleSet[curComp].selectAll('g').attr('transform', function (d) {
+            return 'translate(' + d.x + ',' + d.y + ')';
+        });
+        circleTextSet[curComp].selectAll('g').attr('transform', function (d) {
             return 'translate(' + d.x + ',' + d.y + ')';
         });
     }
@@ -77,25 +81,26 @@ function getanewgraph(filename ,base) {
     }
 
 
-    function startit(filename) {
-        var filteredLinks=linksTot[filename].filter(function(d) { return d.target.name[0]!='h' && d.source.name[0]!='h' && d.util>=thres; });
-        var filteredNodes=d3.values(nodesTot[filename]).filter(function (d){return d.name[0]!='h'});
-        curFile=filename;
+    function startit(filename,whichComp) {
+        var filteredLinks=linksTot[whichComp].filter(function(d) { return d.target.name[0]!='h' && d.source.name[0]!='h' && d.util>=thres; });
+        var filteredNodes=d3.values(nodesTot[whichComp]).filter(function (d){return d.name[0]!='h'});
+        var force=forceSet[whichComp];
+        curComp=whichComp;
 
         // plot the scales
         var scdata = [];
         var tot = 100;
-        var starting=bases[filename];
-        var barlength = 2700;
-        var legWid=60;
-        var scale=scalesSet[filename].selectAll("line").data(scdata,function(d){return d;});
+        var starting=25;
+        var barlength = 600;
+        var legWid=10;
+        var scale=scalesSet[whichComp].selectAll("line").data(scdata,function(d){return d;});
         scale.exit().remove();
         for (var i = 0; i <= tot; i++)
             scdata.push(i * (100 / tot));
-        scale = scalesSet[filename].selectAll("line").data(scdata);
+        scale = scalesSet[whichComp].selectAll("line").data(scdata);
             var sg = scale.enter();
             sg.append('line').style("stroke", function (d) {return getcolor(d); })
-                .style("stroke-width", function (d) {if (thres==d) return 100; else return legWid;})
+                .style("stroke-width", function (d) {if (thres==d) return 20; else return legWid;})
                 .attr("y1", starting)
                 .attr("y2", starting)
                 .style("opacity", function (d) { if (thres==d) return 100; else return getopa(d); })
@@ -103,13 +108,17 @@ function getanewgraph(filename ,base) {
                 .attr("x2", function (d, i) { return 100 + (i + 1) * barlength / tot; })
                 .on('mousedown',function (d){
                     thres=d;
-                    startit(filename);
+                    startit(filename,whichComp);
                 })
                 .append("title")
                 .text(function (d) { return d; });
             scale.exit().remove();
 
-            svgSet[filename].append('svg:text').attr('y', starting-legWid).attr('x',  barlength/2.4).attr('class', 'textid').text("Congestion of "+filename);
+            var textin=["Congestion of "+filename];
+            var curText=svgSet[whichComp].selectAll('text').data(textin, function (d) {return d;});
+            curText.enter().
+              append('text').attr('y', starting-legWid).attr('x',  barlength/2).text(function(d){return d;});
+            curText.exit().remove();
             scale.enter().append('svg:text')
                 .attr('x', function (d, i) { return 100 + i * barlength / tot; })
                 .attr('y', starting+legWid*2)
@@ -122,7 +131,7 @@ function getanewgraph(filename ,base) {
                 });
 
 
-        var path= pathSet[filename].selectAll('path').data(filteredLinks, function (d) {return d.source.name+"-"+d.target.name;});
+        var path= pathSet[whichComp].selectAll('path').data(filteredLinks, function (d) {return d.source.name+"-"+d.target.name;});
 
 
         path.classed('class','links')
@@ -151,21 +160,25 @@ function getanewgraph(filename ,base) {
 
         path.exit().remove();
 
-        var circle = circleSet[filename].selectAll('g').data(filteredNodes, function (d) {return d.name;});
 
-        var g = circle.enter().append("svg:g");
-        g.append('svg:circle')
+        circleTextSet[whichComp].selectAll('g').data([]).exit().remove();
+        circleSet[whichComp].selectAll('g').data([]).exit().remove();
+        var circleText = circleTextSet[whichComp].selectAll('g').data(filteredNodes, function (d) {return d.name;});
+
+        circleText.enter().append('svg:g').append("text")
+            .attr('x', -4)
+            .attr('y', -8)
+            .text(function (d) { return d.name; });
+        circleText.exit().remove();
+
+        var circle = circleSet[whichComp].selectAll('g').data(filteredNodes, function (d) {return d.name;});
+        circle.enter().append('svg:g').append('circle')
             .attr('class', 'node')
             .attr('r', nodeRadius)
             .style('fill', '#000')
             .style('stroke', '#fff')
             .call(force.drag)
             .append("title")
-            .text(function (d) { return d.name; });
-
-        g.append("text")
-            .attr('x', -4)
-            .attr('y', -8)
             .text(function (d) { return d.name; });
         circle.exit().remove();
 
@@ -183,42 +196,81 @@ function getanewgraph(filename ,base) {
             counts[norm] = ++counts[norm] || 1;
             link.norm = norm;
             link.count = counts[norm];
-            link.source = nodes[link.source] || (nodes[link.source] = { name: link.source });
-            link.target = nodes[link.target] || (nodes[link.target] = { name: link.target });
+            link.source = nodes[link.source] || (nodes[link.source] = { name: link.source, fname:filename });
+            link.target = nodes[link.target] || (nodes[link.target] = { name: link.target, fname:filename });
         });
-        force = d3.layout.force()
+        if (forceSet[whichComp])
+            forceSet[whichComp].stop();
+
+        var force = d3.layout.force()
             .nodes(d3.values(nodes))
             .links(links)
             .size([width, height])
-            .linkDistance(50)
-            .charge(-5000)
+            .linkDistance(3)
+            .charge(-1000)
             .on('tick', tick);
-        linksTot[filename]=links;
-        nodesTot[filename]=nodes;
-        bases[filename]=base;
-        var svg = d3.select('body')
+        forceSet[whichComp]=force;
+        linksTot[whichComp]=links;
+        nodesTot[whichComp]=nodes;
+        var svg =svgSet[whichComp]|| d3.select(whichComp)
             .append('svg')
             .attr('oncontextmenu', true)
             .attr('width', width)
             .attr('height', height);
 
-        var path = svg.append('svg:g');
-        var circle = svg.append('svg:g');
-        var scales = svg.append('svg:g');
-        pathSet[filename]=path;
-        circleSet[filename]=circle;
-        scalesSet[filename]=scales;
-        svgSet[filename]=svg;
-        curFile=filename;
-        startit(filename,base);
+        var path = pathSet[whichComp] || svg.append('svg:g');
+        var circle =circleSet[whichComp]|| svg.append('svg:g');
+        var circleText =circleTextSet[whichComp]|| svg.append('svg:g');
+        var scales =scalesSet[whichComp]|| svg.append('svg:g');
+        pathSet[whichComp]=path;
+
+        circleSet[whichComp]=circle;
+        circleTextSet[whichComp]=circleText;
+        scalesSet[whichComp]=scales;
+        svgSet[whichComp]=svg;
+        curComp=whichComp;
+        startit(filename,whichComp);
     });
 }
+var idName='select1';
+var oldSel = document.getElementById(idName);
+while (oldSel.options.length > 1) {
+    oldSel.remove(oldSel.options.length - 1);
+}
+var i=0;
 
 d3.json("files.json", function (error, data) {
     if (error) alert(error);
-    var base=150;
     data.forEach(function (filename) {
-        getanewgraph(filename.name,base);
-        base=base;
+        var opt = document.createElement('option');
+        opt.text = filename.name;
+        opt.value = i;
+        i=i+1;
+
+        oldSel.add(opt, null);
     })
 });
+
+var idName2='select2';
+var oldSel2 = document.getElementById(idName2);
+while (oldSel2.options.length > 1) {
+    oldSel2.remove(oldSel2.options.length - 1);
+}
+var i2=0;
+var rs=[];
+d3.json("files.json", function (error, data) {
+    if (error) alert(error);
+    data.forEach(function (filename) {
+        var opt = document.createElement('option');
+        opt.text = filename.name;
+        opt.value = i2;
+        i2=i2+1;
+        rs.push(filename.name);
+
+        oldSel2.add(opt, null);
+    })
+});
+
+function selectTopo(inobj,which) {
+    getanewgraph(rs[parseInt(inobj)],which);
+}
