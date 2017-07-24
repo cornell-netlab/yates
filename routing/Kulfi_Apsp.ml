51adfe393f404df7ef2b6a28a073df93bf9dc914
Praveen Kumar <praveenk@cs.cornell.edu>
2017-07-23 22:53:45 -0700

    All pairs multiple shortest path - fix case when direct link is not the shortest path

--- routing/Kulfi_Apsp.ml
+++ routing/Kulfi_Apsp.ml
@@ -7,118 +7,117 @@ open Kulfi_Types
 
 module PQueue = Core_kernel.Heap.Removable
 
-(* ------------ All pair multiple shortest paths ------------ *)
+(**************************************************************)
+(* All pair multiple shortest paths *)
+(**************************************************************)
 
-let abs_fl (n:float) =
-  if n > 0.0 then n else -.n
-
-let rec dp_calc_numpaths (i:Topology.vertex) (j:Topology.vertex)
+let rec calc_num_paths (i:Topology.vertex) (j:Topology.vertex)
           (topo:topology) (dist: float SrcDstMap.t)
           (numpath: (bool * int * (Topology.vertex * float) List.t) SrcDstMap.t)
   : (bool * int * (Topology.vertex * float) List.t) SrcDstMap.t =
-  let  (v, n, l) = SrcDstMap.find_exn numpath (i,j) in
-  let n_numpath = SrcDstMap.add numpath ~key:(i,j) ~data:(true, n, l) in
-  let neigh = Topology.neighbors topo i in
-  (* For each vertex *)
-  let n3_numpath =
-    Topology.fold_vertexes
-      (fun nextHop acc ->
-         if nextHop = i then
-           acc
-         else if not (VertexSet.mem neigh nextHop) then
-           acc
-         else
-           (* consider it as next hop if neighbor *)
-           let d_ih = SrcDstMap.find_exn dist (i,nextHop) in
-           let d_hj = SrcDstMap.find_exn dist (nextHop,j) in
-           let d_ij = SrcDstMap.find_exn dist (i,j) in
-           if (abs_fl(d_ih +. d_hj -. d_ij) < 0.00001) then
-             (* if it is in a shortest i-j path *)
-             let t_visited,_,_ = SrcDstMap.find_exn acc  (nextHop,j) in
-             let n2_numpath =
-               if t_visited then acc
-               else dp_calc_numpaths nextHop j topo dist acc  in
-             let _,np_nj,_ = SrcDstMap.find_exn n2_numpath (nextHop,j) in
-             let _,np_ij,l_ij = SrcDstMap.find_exn n2_numpath (i,j) in
-             let t_np_ij = np_ij + np_nj in
-             SrcDstMap.add n2_numpath ~key:(i,j)
-               ~data:(true, t_np_ij,
-                      List.append l_ij [(nextHop, Float.of_int np_nj)])
-           else acc)
-      topo n_numpath in
-  let _,normalizer,l_ij = SrcDstMap.find_exn n3_numpath (i,j) in
+  (* Count the number of shortest i-j paths, and store the corresponding next
+     hop nodes *)
+  let  (_, n, l) = SrcDstMap.find_exn numpath (i,j) in
+  let numpath = SrcDstMap.add numpath ~key:(i,j) ~data:(true, n, l) in
+  let d_ij = SrcDstMap.find_exn dist (i,j) in
+  let neighbors = Topology.neighbors topo i in
+  let numpath =
+    (* For each neighbor, find the shortest paths through it and check if there
+    is a shortest i-j path *)
+    VertexSet.fold neighbors ~init:numpath
+      ~f:(fun acc neighbor ->
+        let out_edge = Topology.find_edge topo i neighbor in
+        let wt_in = Link.weight (Topology.edge_to_label topo out_edge) in
+        let d_nj = SrcDstMap.find_exn dist (neighbor,j) in
+        if wt_in +. d_nj = d_ij then
+          (* if it is in a shortest i-j path *)
+          let t_visited,_,_ = SrcDstMap.find_exn acc (neighbor,j) in
+          let numpath =
+            if t_visited then acc
+            else calc_num_paths neighbor j topo dist acc  in
+          let _, np_nj, _ = SrcDstMap.find_exn numpath (neighbor,j) in
+          let _, np_ij, next_hops_ij = SrcDstMap.find_exn numpath (i,j) in
+          (* Extend i-j paths by number of shortest paths through this neighbor *)
+          SrcDstMap.add numpath ~key:(i,j)
+            ~data:(true, np_ij + np_nj,
+                   List.append next_hops_ij [(neighbor, Float.of_int np_nj)])
+        else acc) in
+
+  let _, num_ij_paths, next_hops_ij = SrcDstMap.find_exn numpath (i,j) in
   let path_probs,_ =
-    List.fold_left l_ij
+    List.fold_left next_hops_ij
       ~init:([], 0.0)
-      ~f:(fun acc (nextHop, np_h) ->
+      ~f:(fun acc (next_hop, np_h) ->
         let l, preprob = acc in
-        let n_prob = np_h /. Float.of_int normalizer in
-        (List.append l [(nextHop, preprob +. n_prob)], preprob +. n_prob)) in
-  SrcDstMap.add n3_numpath ~key:(i,j) ~data:(true, normalizer, path_probs)
+        let n_prob = np_h /. Float.of_int num_ij_paths in
+        (List.append l [(next_hop, preprob +. n_prob)], preprob +. n_prob)) in
+  SrcDstMap.add numpath ~key:(i,j) ~data:(true, num_ij_paths, path_probs)
 
 
 let all_pairs_multi_shortest_path (topo:topology) :
   (bool * int * (Topology.vertex * float) List.t) SrcDstMap.t =
   (* topology -> (visited_bool, normalizer, list(next_hop, prob)) SrcDstMap *)
+
   let dist_mat = Topology.fold_vertexes
-    (fun i dist_mat -> Topology.fold_vertexes
-        (fun j dist_mat2 ->
-          let ans = if (i=j) then 0.0
-          else Float.infinity in
-          SrcDstMap.add dist_mat2 ~key:(i,j) ~data:ans)
+    (fun i acc -> Topology.fold_vertexes
+        (fun j acc ->
+          let dist =
+            if i = j then 0.0
+            else Float.infinity in
+          SrcDstMap.add acc ~key:(i, j) ~data:dist)
         topo
-        dist_mat)
+        acc)
     topo
     SrcDstMap.empty in
 
   (* Floyd-Warshall *)
-  let dist_mat_init = Topology.fold_edges
+  let dist_mat = Topology.fold_edges
   (fun e acc ->
     let src,_ = Topology.edge_src e in
     let dst,_ = Topology.edge_dst e in
     let weight = Link.weight (Topology.edge_to_label topo e) in
     SrcDstMap.add acc ~key:(src, dst) ~data:weight)
   topo dist_mat in
+
   let dist_mat_sp = Topology.fold_vertexes
     (fun k acc ->
       Topology.fold_vertexes
-        (fun i acc_i ->
+        (fun i acc ->
           Topology.fold_vertexes
-          (fun j acc_j ->
-            let dij  = SrcDstMap.find_exn acc_j (i,j)  in
-            let dik  = SrcDstMap.find_exn acc_j (i,k)  in
-            let dkj  = SrcDstMap.find_exn acc_j (k,j)  in
-            let upd_val =
-              if (dik +. dkj < dij) then dik +. dkj
-              else dij in
-            SrcDstMap.add acc_j ~key:(i,j) ~data:upd_val)
-          topo acc_i)
+          (fun j acc ->
+            let dij = SrcDstMap.find_exn acc (i,j)  in
+            let dik = SrcDstMap.find_exn acc (i,k)  in
+            let dkj = SrcDstMap.find_exn acc (k,j)  in
+            if (dik +. dkj < dij) then
+              SrcDstMap.add acc ~key:(i, j) ~data:(dik +. dkj)
+            else acc)
+          topo acc)
         topo acc)
-    topo dist_mat_init in
+    topo dist_mat in
   (* Floyd-Warshall complete *)
 
   (* initialize visited to be true for i,i *)
   let init_vis_npath_pathlist_map =
     Topology.fold_vertexes
-      (fun i acc_i ->
+      (fun i acc ->
          Topology.fold_vertexes
-           (fun j acc_j ->
-              let visited = if (i=j) then true else false in
-              let num_paths = if (i=j) then 1 else 0 in
-              SrcDstMap.add acc_j ~key:(i,j) ~data:(visited,num_paths,[]))
-           topo acc_i)
+           (fun j acc ->
+              (* if i == j, then visited is true, and num_paths = 1 *)
+              let visited = (i = j) in
+              let num_paths = if (i = j) then 1 else 0 in
+              SrcDstMap.add acc ~key:(i, j) ~data:(visited, num_paths, []))
+           topo acc)
       topo SrcDstMap.empty in
-  let numPath_map =
-    Topology.fold_vertexes
-      (fun i acc_i ->
-         Topology.fold_vertexes
-           (fun j acc_j ->
-              let visited,np,_ = SrcDstMap.find_exn acc_j (i,j) in
-              if (visited) then acc_j
-              else dp_calc_numpaths i j topo dist_mat_sp acc_j)
-           topo acc_i)
-      topo init_vis_npath_pathlist_map in
-  numPath_map
+
+  Topology.fold_vertexes
+    (fun i acc ->
+       Topology.fold_vertexes
+         (fun j acc ->
+            let visited,np,_ = SrcDstMap.find_exn acc (i,j) in
+            if visited then acc
+            else calc_num_paths i j topo dist_mat_sp acc)
+         topo acc)
+    topo init_vis_npath_pathlist_map
 
 let print_mpapsp
   (numpath : (bool * int * (Topology.vertex * float) List.t) SrcDstMap.t)
@@ -171,10 +170,12 @@ let get_random_path (i:Topology.vertex) (j:Topology.vertex) (topo:topology)
   !e_path
 
 
-(*******************  k-shortest paths *************************************)
+(**************************************************************)
+(* k-shortest paths *)
+(**************************************************************)
 
-let k_shortest_path (topo:topology) (s:Topology.vertex) (t:Topology.vertex) (k:int) =
-  (* k-shortest s,t paths - Yen's algorithm *)
+let k_shortest_path (topo:topology) (s:Topology.vertex) (t:Topology.vertex)
+      (k:int) : path list =
   if s = t then []
   else
     let paths = ref [] in
@@ -182,7 +183,7 @@ let k_shortest_path (topo:topology) (s:Topology.vertex) (t:Topology.vertex) (k:i
     Topology.iter_vertexes
       (fun u -> Hashtbl.Poly.add_exn count u 0;) topo;
 
-    let bheap =
+    let bheap = (* store paths, priority = cost of path *)
       PQueue.create
         ~min_size:(Topology.num_vertexes topo)
         ~cmp:(fun (dist1,_) (dist2,_) -> compare dist1 dist2) () in
@@ -218,62 +219,32 @@ let k_shortest_path (topo:topology) (s:Topology.vertex) (t:Topology.vertex) (k:i
           else if u = t then ()
           else explore () in
     explore ();
-  (*
-  Printf.printf "Num paths %d" (List.length !paths);
-  let _ = List.fold_left
-    !paths
-    ~init:[]
-    ~f:(fun acc path ->
-      Printf.printf "\n";
-      List.fold_left (List.rev path)
-      ~init:[]
-      ~f:(fun acc v ->
-        Printf.printf "%s\t" (Node.name (Net.Topology.vertex_to_label topo v));
-        acc)) in
-  *)
-    let edge_paths =
-      List.fold_left !paths
-        ~init:[]
-        ~f:(fun acc path ->
-          let edge_path =
-            List.fold_left path
-              ~init:([], None)
-              ~f:(fun edges_u v ->
-                let edges, u = edges_u in
-                match u with
-                | None ->
-                  (edges, Some v)
-                | Some u ->
-                  let edge = Topology.find_edge topo v u in
-                  (edge::edges, Some v)) in
-          let p,_ = edge_path in
-          let p' =
-            if !Kulfi_Globals.deloop then Kulfi_Frt.FRT.remove_cycles p
-            else p in
-          p'::acc) in
- (*
-  List.iter
-    edge_paths
-    ~f:(fun path -> Printf.printf "\n";
-        Printf.printf "%s -> " (Node.name (Net.Topology.vertex_to_label topo s));
-        Printf.printf "%s\t" (Node.name (Net.Topology.vertex_to_label topo t));
-        List.iter
-        path
-        ~f:(fun edge ->
-          let (dst,_) = Topology.edge_dst edge in
-          Printf.printf "%s\t" (Node.name (Net.Topology.vertex_to_label topo dst))));
-  *)
-  edge_paths
+
+    (* Convert paths from list of nodes to list of edges *)
+    List.fold_left !paths ~init:[]
+      ~f:(fun acc path ->
+        let edge_path =
+          List.fold_left path ~init:([], None)
+            ~f:(fun edges_u v ->
+              let edges, u = edges_u in
+              match u with
+              | None ->
+                (edges, Some v)
+              | Some u ->
+                let edge = Topology.find_edge topo v u in
+                (edge::edges, Some v)) in
+        let p,_ = edge_path in
+        let p' =
+          if !Kulfi_Globals.deloop then
+            Kulfi_Frt.FRT.remove_cycles p
+          else p in
+        p'::acc)
 (* end k-shortest path *)
 
 let all_pair_k_shortest_path (topo:topology) (k:int) hosts =
-  VertexSet.fold
-    hosts
-    ~init:SrcDstMap.empty
+  VertexSet.fold hosts ~init:SrcDstMap.empty
     ~f:(fun acc src ->
-      VertexSet.fold
-        hosts
-        ~init:acc
-        ~f:(fun nacc dst ->
+      VertexSet.fold hosts ~init:acc
+        ~f:(fun acc dst ->
           let ksp = k_shortest_path topo src dst k in
-          SrcDstMap.add nacc ~key:(src, dst) ~data:ksp))
+          SrcDstMap.add acc ~key:(src, dst) ~data:ksp))
--- routing/Kulfi_Ecmp.ml
+++ routing/Kulfi_Ecmp.ml
@@ -9,104 +9,87 @@ open Kulfi_Util
 
 let prev_scheme = ref SrcDstMap.empty
 
-let rec getAllSp (v1:Topology.vertex) (v2:Topology.vertex) (topo:topology) (apsp: (bool * int * (Topology.vertex * float) List.t)
-SrcDstMap.t) (max_tot: int) : (Topology.edge List.t) List.t=
-  if (v1=v2) then
-    [[]]
+let rec get_all_shortest_paths (u:Topology.vertex) (v:Topology.vertex)
+          (topo:topology)
+          (apsp: (bool * int * (Topology.vertex * float) List.t) SrcDstMap.t)
+          (max_num_paths: int) : (Topology.edge List.t) List.t =
+  if u = v then [[]]
   else
-    let _,_,nhop_list= SrcDstMap.find_exn apsp (v1, v2) in
-    let p1=List.fold_left nhop_list
-        ~init: []
-        ~f:
-        (fun acc (nhop,_)  ->
-          let remains=max_tot-(List.length acc) in
-          if remains<0 then
+    let _,_,nhop_list= SrcDstMap.find_exn apsp (u, v) in
+    List.fold_left nhop_list ~init: []
+        ~f:(fun acc (nhop,_)  ->
+          let remains = max_num_paths - (List.length acc) in
+          if remains < 0 then
             acc
           else
-            let nextPaths=getAllSp nhop v2 topo apsp remains in
-            let myans=List.fold_left nextPaths
-              ~init: []
-              ~f:(fun acc2 path ->
-                let thisedge=Topology.find_edge topo v1 nhop in
-                let newp=List.append [thisedge] path in
-                  List.append acc2 [newp]
-              ) in
-            let newacc=List.append acc myans in
-              newacc
-        ) in
-    p1
+            let nh_edge = Topology.find_edge topo u nhop in
+            let nh_paths = get_all_shortest_paths nhop v topo apsp remains in
+            List.fold_left nh_paths
+              ~init:acc
+              ~f:(fun acc nh_path ->
+                (nh_edge::nh_path)::acc))
 
 let solve_thresh (topo:topology) (_:demands) : scheme =
-  let new_scheme =
-  if not (SrcDstMap.is_empty !prev_scheme) then !prev_scheme
-  else
-  (* (pk): run ksp with budget;
-   * find shortest path;
+  (* Not used *)
+  (* run ksp with budget; find shortest path;
    * select paths with weight <= 1.2 times shortest *)
-  let host_set = get_hosts_set topo in
-  let all_ksp = all_pair_k_shortest_path topo (min !Kulfi_Globals.budget 100) host_set in
-  let thresh = 1.2 in
-  SrcDstMap.fold all_ksp
-    ~init: SrcDstMap.empty
-    ~f:(fun ~key:(u,v) ~data:paths acc ->
-      if (u = v) then acc
-      else
-        if List.is_empty paths then acc
-        else
-          let sorted_paths = List.sort ~cmp:(fun x y -> Pervasives.compare (get_path_weight topo x) (get_path_weight topo y)) paths in
-          let shortest_path_weight = get_path_weight topo (match List.hd sorted_paths with
-              | None -> assert false
-              | Some x -> x) in
-          (*Printf.printf "Shortest path weight: %f\n" shortest_path_weight;*)
-          let selected_paths = List.filter sorted_paths
-            ~f:(fun p -> (get_path_weight topo p) <= (thresh *. shortest_path_weight)) in
-          (*List.iter selected_paths ~f:(fun p ->
-            Printf.printf "%f : %s\n%!" (get_path_weight topo p) (dump_edges topo p);); *)
-          let prob = 1. /. Float.of_int (List.length selected_paths) in
-          let path_dist = List.fold_left selected_paths ~init: PathMap.empty
-                    ~f:(fun acc p -> PathMap.add ~key:p ~data:prob acc) in
-          SrcDstMap.add ~key:(u,v) ~data:path_dist acc) in
+  let new_scheme =
+    if not (SrcDstMap.is_empty !prev_scheme) then !prev_scheme
+    else
+      let host_set = get_hosts_set topo in
+      let all_ksp =
+        all_pair_k_shortest_path topo (min !Kulfi_Globals.budget 100) host_set in
+      let thresh = 1.2 in
+      SrcDstMap.fold all_ksp
+        ~init: SrcDstMap.empty
+        ~f:(fun ~key:(u,v) ~data:paths acc ->
+          if (u = v) then acc
+          else
+          if List.is_empty paths then acc
+          else
+            let sorted_paths = List.sort ~cmp:(fun x y -> Pervasives.compare (get_path_weight topo x) (get_path_weight topo y)) paths in
+            let shortest_path_weight = get_path_weight topo
+                                         (match List.hd sorted_paths with
+                                          | None -> assert false
+                                          | Some x -> x) in
+            let selected_paths =
+              List.filter sorted_paths
+                ~f:(fun p ->
+                  (get_path_weight topo p) <= (thresh *. shortest_path_weight)) in
+            let prob = 1. /. Float.of_int (List.length selected_paths) in
+            let path_dist =
+              List.fold_left selected_paths ~init: PathMap.empty
+                ~f:(fun acc p ->
+                  PathMap.add ~key:p ~data:prob acc) in
+            SrcDstMap.add ~key:(u,v) ~data:path_dist acc) in
   prev_scheme := new_scheme;
   new_scheme
 
 
 let solve (topo:topology) (_:demands) : scheme =
   let new_scheme =
-  if not (SrcDstMap.is_empty !prev_scheme) then !prev_scheme
-  else
-  (*
-   * Yang: my implementation is the following:
-     * If there are less than max_tot=1000 different shortest paths between s and t,
-     * we will pick all of them, and assign 1.0/tot prob
-     * If there are more than max_tot=1000 different shortest paths between s and t,
-     * we simply pick random 1000 of the shortest paths
-   *)
-  let mpapsp=all_pairs_multi_shortest_path topo in
-  let host_set =
-    VertexSet.filter
-      (vertexes topo)
-      ~f:(fun v ->
-          let lbl = vertex_to_label topo v in
-          Node.device lbl = Node.Host) in
+    if not (SrcDstMap.is_empty !prev_scheme) then !prev_scheme
+    else
+      (* Compute all pair multiple shortest paths, *)
+      (* For each next hop in that, reconstruct shortest paths recursively  *)
+      let apmsp = all_pairs_multi_shortest_path topo in
+      let host_set = get_hosts_set topo in
 
-  let max_tot=20 in
-  SrcDstMap.fold
-        mpapsp
-        ~init: SrcDstMap.empty
-        ~f:(fun ~key:(v1,v2) ~data:(_,n,probs) acc ->
-              if (VertexSet.mem host_set v1) && (VertexSet.mem host_set v2) then
-                let paths=getAllSp v1 v2 topo mpapsp max_tot in
-                let prob=1.0 /. Float.of_int (List.length paths) in
-                let path_dist= List.fold_left paths
-                  ~init: PathMap.empty
-                  ~f:( fun acc path ->
-                    PathMap.add acc ~key:path ~data:prob;) in
-                  SrcDstMap.add acc ~key:(v1,v2) ~data: path_dist
-              else
-                acc
-        ) in
-  (*Printf.printf "Done calculating paths %!";*)
-  (*Printf.printf "%s" (dump_scheme topo sp_table);*)
+      (* Limit number of paths to 100 or budget, whichever is minimum *)
+      let max_num_paths = min !Kulfi_Globals.budget 100 in
+      SrcDstMap.fold apmsp ~init: SrcDstMap.empty
+        ~f:(fun ~key:(u, v) ~data:(_,n,probs) acc ->
+          if (VertexSet.mem host_set u) && (VertexSet.mem host_set v) then
+            let paths = get_all_shortest_paths u v topo apmsp max_num_paths in
+            let prob = 1.0 /. Float.of_int (List.length paths) in
+            let path_dist =
+              List.fold_left paths
+                ~init: PathMap.empty
+                ~f:( fun acc path ->
+                  PathMap.add acc ~key:path ~data:prob;) in
+            SrcDstMap.add acc ~key:(u, v) ~data: path_dist
+          else
+            acc) in
   prev_scheme := new_scheme;
   new_scheme
 
