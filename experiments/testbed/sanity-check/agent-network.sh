5f1fb71f722855081edd6b35af4ddf2431863110
Praveen Kumar <praveenk@cs.cornell.edu>
2018-05-04 21:10:43 -0400

    Remove experiment scripts

--- experiments/testbed/sanity-check/agent-network.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in `seq 1 12`; do
-    ping -c 1 10.0.0.$i
-done
--- experiments/testbed/sanity-check/uptime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-. ~/utils/colors.sh
-. ~/utils/common.sh
-echo $BLUE"Check uptime of nodes"$RESTORE
-dsh -M -g atlas-abilene -c "uptime"
