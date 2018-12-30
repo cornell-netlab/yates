29f2e261ca6b52d7c41c2fdd0c81ed5302d33718
Praveen Kumar <praveenk@cs.cornell.edu>
2018-12-29 23:36:49 -0500

    Add comments

--- simulator/Simulation_Types.ml
+++ simulator/Simulation_Types.ml
@@ -2,6 +2,7 @@ open Core
 open Yates_routing
 open Yates_types.Types
 
+(* State used during simulation of a single TM *)
 type network_iter_state = {
   ingress_link_traffic  : ((edge Array.t * int * float) list) EdgeMap.t;
   delivered       : float SrcDstMap.t;
@@ -15,6 +16,7 @@ type network_iter_state = {
   predict_tm      : demands;
 }
 
+(* Statistics returned after simulating each TM *)
 type sim_tm_stats = {
   throughput    : throughput SrcDstMap.t;
   latency       : throughput LatencyMap.t;
